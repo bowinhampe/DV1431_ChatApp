90a7dd2e04c17d60e81d352e78093b7a8fb55f80
bowinHampe <hampusbowin@gmail.co>
2017-11-22 16:03:59 +0100

    Added: Login fragment, Register fragment

--- app/src/main/java/com/example/hampus/dv1431_chatapp/LoginActivity.kt
+++ app/src/main/java/com/example/hampus/dv1431_chatapp/LoginActivity.kt
@@ -1,35 +1,31 @@
 package com.dv1431_chatapp
 
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.hampus.dv1431_chatapp.LoginFragment
+import com.example.hampus.dv1431_chatapp.RegisterFragment
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_login.view.*
 
 class LoginActivity : AppCompatActivity() {
+
+    val mManager = supportFragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-        initializeGUIComponents()
+        initiateGUIComponents()
     }
 
-    private fun initializeGUIComponents() {
-        loginActivity_login_btn.setOnClickListener {
-            // Start the main app activity
-            login()
-        }
-        loginActivity_register_btn.setOnClickListener {
-            // Start a register Fragment
-            register()
-        }
+    private fun initiateGUIComponents(){
+        val transaction = mManager.beginTransaction()
+        val loginFragment = LoginFragment()
+        transaction.replace(loginActivity_mainLayout.id, loginFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 
-    private fun login() {
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-    }
-    private fun register(){
-
-    }
 }
---
+++ app/src/main/java/com/example/hampus/dv1431_chatapp/LoginFragment.kt
@@ -0,0 +1,113 @@
+package com.example.hampus.dv1431_chatapp
+
+import android.app.FragmentManager
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dv1431_chatapp.MainActivity
+import kotlinx.android.synthetic.main.fragment_login.view.*
+
+import com.dv1431_chatapp.R
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.fragment_login.*
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [LoginFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment() {
+
+    // TODO: Rename and change types of parameters
+    private var mParam1: String? = null
+    private var mParam2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (arguments != null) {
+            mParam1 = arguments.getString(ARG_PARAM1)
+            mParam2 = arguments.getString(ARG_PARAM2)
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initializeGUIComponents()
+    }
+
+    private fun initializeGUIComponents() {
+        loginFragment_login_btn.setOnClickListener {
+            // Start the main app activity
+            login()
+        }
+        loginFragment_register_btn.setOnClickListener {
+            // Start a register Fragment
+            register()
+        }
+    }
+
+    private fun login() {
+        val intent = Intent(context, MainActivity::class.java)
+        startActivity(intent)
+    }
+    private fun register(){
+        val manager = childFragmentManager
+        val transaction = manager.beginTransaction()
+        val registerFragment = RegisterFragment()
+        transaction.replace(loginActivity_mainLayout.id, registerFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater!!.inflate(R.layout.fragment_login, container, false)
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    fun onButtonPressed(uri: Uri) {
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+
+    companion object {
+        // TODO: Rename parameter arguments, choose names that match
+        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+        private val ARG_PARAM1 = "param1"
+        private val ARG_PARAM2 = "param2"
+
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment LoginFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        fun newInstance(param1: String, param2: String): LoginFragment {
+            val fragment = LoginFragment()
+            val args = Bundle()
+            args.putString(ARG_PARAM1, param1)
+            args.putString(ARG_PARAM2, param2)
+            fragment.arguments = args
+            return fragment
+        }
+    }
+}// Required empty public constructor
