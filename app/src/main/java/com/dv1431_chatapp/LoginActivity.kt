961f311e20340d1988993c81d276d699426e98f8
Daniel Johansson <daniel.j-@hotmail.com>
2017-11-28 12:19:03 +0100

    Changed: Now signs in and retrieves userdata with the help of the Database class

--- app/src/main/java/com/dv1431_chatapp/LoginActivity.kt
+++ app/src/main/java/com/dv1431_chatapp/LoginActivity.kt
@@ -1,27 +1,34 @@
 package com.dv1431_chatapp
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.Toast
-
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class LoginActivity : AppCompatActivity() {
 
     // For logging
     private val TAG = LoginActivity::class.java.simpleName as String
 
-    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mProgressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         initiateGUIComponents()
-
-        mAuth = FirebaseAuth.getInstance()
     }
 
     private fun initiateGUIComponents(){
@@ -33,28 +40,61 @@ class LoginActivity : AppCompatActivity() {
             // Start a register Fragment
             register()
         }
+
+        mProgressBar = ProgressBar(this)
+        mProgressBar.isIndeterminate = true
     }
 
     private fun login() {
         val email = loginActivity_usrname_edtxt.text.toString()
         val password = loginActivity_pw_edtxt.text.toString()
 
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "signInWithEmail:success")
-                        val userId = mAuth.currentUser?.uid
-                        val intent = Intent(this, MainActivity::class.java)
-                        intent.putExtra(MainActivity.EXTRAS_USER_ID, userId)
-                        startActivity(intent)
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "signInWithEmail:failure", task.exception)
-                        Toast.makeText(this, "Authentication failed.",
-                                Toast.LENGTH_LONG).show()
-                    }
-        }
+        val context = this
+
+        Database().login(email, password, object : OnCompleteListener {
+            override fun onStart() {
+                mProgressBar.visibility = View.VISIBLE
+            }
+
+            override fun onSuccess(task: Task<AuthResult>) {
+                retrieveUserFromDatabase(task.result.user.uid)
+            }
+
+            override fun onFail(task: Task<AuthResult>) {
+                Toast.makeText(context, "Authentication failed.",
+                        Toast.LENGTH_LONG).show()
+            }
+
+        })
+    }
+
+    private fun retrieveUserFromDatabase(userId: String) {
+        val context = this
+
+        Database().retrieveDataOnce("users/"+userId, object : OnDataChangeListener {
+            override fun onStart() {
+            }
+
+            override fun onChange(dataSnapshot: DataSnapshot) {
+                val user = dataSnapshot.getValue<User>(User::class.java)//.getValue(User::class.java)
+
+                if (user != null) {
+                    user.setId(dataSnapshot.key)
+                    val intent = Intent(context, MainActivity::class.java)
+                    intent.putExtra(User::class.java.simpleName, user)
+                    mProgressBar.visibility = View.GONE
+                    startActivity(intent)
+                } else {
+                    Log.w(TAG, "retrieveUserFromDatabase:failure")
+                    Toast.makeText(context, "An error occurred. PLease try again.",
+                            Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                //DO SOME THING WHEN GET DATA FAILED HERE
+            }
+        })
     }
 
     private fun register(){
