5123e7db2ebd382084370134518af6c030ddf8e4
Daniel Johansson <daniel.j-@hotmail.com>
2017-11-28 12:19:54 +0100

    Changed: Now registers users with the help of the Database class

--- app/src/main/java/com/dv1431_chatapp/RegisterActivity.kt
+++ app/src/main/java/com/dv1431_chatapp/RegisterActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_register.*
 import android.widget.Toast
 import android.util.Log
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 
@@ -25,7 +27,6 @@ class RegisterActivity : AppCompatActivity() {
 
     private fun initiateGUIComponents(){
         registerActivity_register_btn.setOnClickListener {
-            // Start a register Fragment
             register()
         }
     }
@@ -37,38 +38,32 @@ class RegisterActivity : AppCompatActivity() {
 
         //validateCredentials()
 
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "createUserWithEmail:success")
+        val context = this
 
-                        val fbUser = mAuth.currentUser
-                        if (fbUser != null) {
-                            val user = User(fbUser.uid, email, username)
-                            addUserToDatabase(user)
-                            val intent = Intent(this, MainActivity::class.java)
-                            //intent.putExtra(MainActivity.EXTRAS_USER_ID, user.getId())
-                            intent.putExtra(User::class.java.simpleName, user)
-                            startActivity(intent)
-                        } else {
-                            Log.w(TAG, "createUserWithEmail:failure")
-                            Toast.makeText(this, "An error occurred. Please try again.",
-                                    Toast.LENGTH_LONG).show()
-                        }
+        Database().register(email, password, username, object : OnCompleteListener{
+            override fun onStart() {
 
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                        Toast.makeText(this, "Authentication failed.",
-                                Toast.LENGTH_LONG).show()
-                    }
-                }
+            }
+
+            override fun onSuccess(task: Task<AuthResult>) {
+                val user = User(task.result.user.uid, email, username)
+                addUserToDatabase(user)
+                val intent = Intent(context, MainActivity::class.java)
+                intent.putExtra(User::class.java.simpleName, user)
+                startActivity(intent)
+            }
+
+            override fun onFail(task: Task<AuthResult>) {
+                Toast.makeText(context, "Authentication failed.",
+                        Toast.LENGTH_LONG).show()
+            }
+
+        })
     }
 
     private fun addUserToDatabase(user: User) {
-        val dbUsersRef = FirebaseDatabase.getInstance().getReference("users")
-        dbUsersRef.child(user.getId()).setValue(user)
+        val usersRef = FirebaseDatabase.getInstance().getReference("users")
+        usersRef.child(user.getId()).setValue(user)
     }
 
 }
