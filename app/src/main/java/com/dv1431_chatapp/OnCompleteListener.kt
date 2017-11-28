6a013b1aea63a019c0922fc162a2e119ba8bf359
Daniel Johansson <daniel.j-@hotmail.com>
2017-11-28 12:16:01 +0100

    Added: class Database and two interfaces for handling the firebase database

---
+++ app/src/main/java/com/dv1431_chatapp/OnCompleteListener.kt
@@ -0,0 +1,14 @@
+package com.dv1431_chatapp
+
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+
+/**
+ * Created by dane on 11/28/17.
+ */
+
+interface OnCompleteListener {
+    fun onStart()
+    fun onSuccess(task: Task<AuthResult>)
+    fun onFail(task: Task<AuthResult>)
+}
